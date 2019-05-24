6cec6d2376f40469e36bfe05af4f8c95a4a0b4c7
claraTomasini <49305663+claraTomasini@users.noreply.github.com>
2019-05-24 08:43:55 +0200

    Nettoyage code + commentaires

--- src/pack/Professionnel.java
+++ src/pack/Professionnel.java
@@ -18,18 +18,15 @@ public class Professionnel {
 	/** Telephone **/
 	@Column
 	private String tel;
-	/** Services proposes **/
-	/**@Column
-	private List<Object> services;*/
 	
 	@OneToMany(mappedBy="pro", fetch = FetchType.EAGER)
 	Set<Activite> listeActivite;
 	
 	@OneToMany(mappedBy="pro", fetch = FetchType.EAGER)
 	Set<Avis> listeAvis;
-	
-	
+	/** Constructeur de base **/
 	public Professionnel(){}
+	// Setter,getter
 	public void setName(String name){
 		this.name=name;
 	}
@@ -48,13 +45,4 @@ public class Professionnel {
 	public String getTel(){
 		return this.tel;
 	}
-	/**public void setServ(List<Object> serv){
-		this.services = serv;
-	}
-	public List<Object> getServ(){
-		return this.services;
-	}
-	public void addServ(Object serv){
-		this.services.add(serv);
-	}*/
 }
